5ed42b23dd925a0c71da8daa5b2525719fd9d6cd
haechan9 <162085859+haechan9@users.noreply.github.com>
2024-05-21 15:40:48 +0900

    Week2 과제 제출

---
+++ Week2/script.js
@@ -0,0 +1,14 @@
+var hw = document.getElementById('hw');
+
+hw.addEventListener('click', function()
+{
+    const New=document.getElementById('as');
+    New.innerHTML='<div style="color:red"> red </div>'
+})
+var hw = document.getElementById('hb');
+
+hw.addEventListener('click', function()
+{
+    const New=document.getElementById('as');
+    New.innerHTML='<div style="color:blue"> blue </div>'
+})
